08801fbdbf6816366513a0e80f92cfbfc235e1a4
deepanshu136 <deepanshukumar280@gmail.com>
2024-05-06 11:37:56 +0530

    food section completed

--- controllers/foodController.js
+++ controllers/foodController.js
@@ -76,3 +76,158 @@ module.exports.getAllFoodsController = async (req, res) => {
     });
   }
 };
+
+//get single food by id
+module.exports.getSinleFoodController = async (req, res) => {
+  try {
+    const foodId = req.params.id;
+    if (!foodId) {
+      return res.status(404).send({
+        success: false,
+        message: "please provide  id",
+      });
+    }
+    const food = await foodModel.findById(foodId);
+    //validation
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "no food exist with this id",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "Succesfull",
+      food,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      success: "false",
+      message: "Error in get single food api",
+    });
+  }
+};
+
+//get food by restaurent
+module.exports.getFoodByRestaurent = async (req, res) => {
+  try {
+    const restaurentId = req.params.id;
+    if (!restaurentId) {
+      return res.status(404).send({
+        success: false,
+        message: "Please provide restaurent id",
+      });
+    }
+    const food = await foodModel.find({ restaurent: "restaurentId" });
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "no food found with this id",
+      });
+    }
+    res.status(200).send({
+      success: true,
+      message: "food base on restuatrn",
+      food,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      success: "false",
+      message: "eroor in get by restaurent APi",
+      err: err,
+    });
+  }
+};
+
+//update food item
+module.exports.updateFoodController = async (req, res) => {
+  try {
+    const foodId = req.params.id;
+    if (!foodId) {
+      return res.status(404).send({
+        success: false,
+        message: "no food id was found",
+      });
+    }
+    const food = await foodModel.findById(foodId);
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "No Food Found",
+      });
+    }
+    const {
+      title,
+      description,
+      price,
+      imageUrl,
+      foodTags,
+      category,
+      code,
+      isAvailabe,
+      restaurent,
+      rating,
+    } = req.body;
+    const updatedFood = await foodModel.findByIdAndUpdate(
+      foodId,
+      {
+        title,
+        description,
+        price,
+        imageUrl,
+        foodTags,
+        category,
+        code,
+        isAvailabe,
+        restaurent,
+        rating,
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Food Item Was Updated",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      success: false,
+      message: "Error in update food API",
+      err: err,
+    });
+  }
+};
+
+//delete food
+module.exports.deleteFoodController = async (req, res) => {
+  try {
+    const foodId = req.params.id;
+    if (!foodId) {
+      return res.status(404).send({
+        success: false,
+        message: "Provide food id",
+      });
+    }
+    const food = await foodModel.findById(foodId);
+    if (!food) {
+      return res.status(404).send({
+        success: false,
+        message: "No food found bu id",
+      });
+    }
+    await foodModel.findByIdAndDelete(foodId);
+    res.status(200).send({
+      success: true,
+      message: "Food Item Deleted",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      success: false,
+      message: "Error in Delete Food Api",
+      err: err,
+    });
+  }
+};
--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -3,6 +3,10 @@ const authMiddleware = require("../middelwares/authMiddleware");
 const {
   createFoodController,
   getAllFoodsController,
+  getSinleFoodController,
+  getFoodByRestaurent,
+  updateFoodController,
+  deleteFoodController,
 } = require("../controllers/foodController");
 const router = express.Router();
 
@@ -11,5 +15,13 @@ const router = express.Router();
 router.post("/create", authMiddleware, createFoodController);
 //get all foods
 router.get("/getAll", getAllFoodsController);
+//get single food by id
+router.get("/get/:id", getSinleFoodController);
+//get  food byrestaurent
+router.get("/getByRestaurent/:id", getFoodByRestaurent);
+//update food item
+router.put("/update/:id", authMiddleware, updateFoodController);
+//delete food item
+router.delete("/delete/:id", authMiddleware, deleteFoodController);
 //export
 module.exports = router;
